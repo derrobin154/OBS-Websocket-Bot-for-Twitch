be9961b189a7e90a160045ba33b784751a02859a
derrobin154 <derrobin154@gmail.com>
2021-01-10 20:03:15 +0100

    Version 2.1 - Bugfix: When context.badges is null then crash the Bot - Code Optimizing

--- commands/camera.js
+++ commands/camera.js
@@ -3,25 +3,15 @@ const camerapositions = require("../camerapos.json");
 
 exports.run = (client, message, args, context, channel, self) => {
     if (args.length === 0) return;
-    if (context.mod || context.badges["broadcaster"] === "1") {
-        if (camerapositions[args] !== undefined) {
-            obs.sendCallback('SetSceneItemProperties', {
-                "item": process.env.OBS_ITEMCAMERA,
-                "position": {"x": camerapositions[args].x, "y": camerapositions[args].y}
-            }, (err, data) => {
-                if (err && err.status === "error" && err.error === "specified scene item doesn't exist") {
-                    client.say(process.env.KANALNAME, "Diese Quelle existiert nicht.");
-                    return false;
-                }
-                if (data && data.status === "ok") {
-                    return client.say(process.env.KANALNAME, `Neue Kameraposition: ${args}`)
-                }
-            });
-        } else {
-            client.say(process.env.KANALNAME, "Diese Position wurde nicht festgelegt.");
-            return false;
-        }
-
+    if (context.badges !== null && context.badges["broadcaster"] === "1" || context.badges !== null && context.badges["moderator"] === "1") {
+        if (camerapositions[args] === undefined) return client.say(process.env.KANALNAME, "Diese Position wurde nicht festgelegt.");
+        obs.sendCallback('SetSceneItemProperties', {
+            "item": process.env.OBS_ITEMCAMERA,
+            "position": {"x": camerapositions[args].x, "y": camerapositions[args].y}
+        }, (err, data) => {
+            if (err && err.status === "error" && err.error === "specified scene item doesn't exist") return client.say(process.env.KANALNAME, "Diese Quelle existiert nicht.");
+            if (data && data.status === "ok") return client.say(process.env.KANALNAME, `Neue Kameraposition: ${args}`)
+        });
         return true;
     }
 
--- commands/camerapos.js
+++ commands/camerapos.js
@@ -1,15 +1,10 @@
 const {obs} = require("../bot.js")
 
 exports.run = (client, message, args, context, channel, self) => {
-    if (context.mod || context.badges["broadcaster"] === "1") {
+    if (context.badges !== null && context.badges["broadcaster"] === "1" || context.badges !== null && context.badges["moderator"] === "1") {
         obs.sendCallback('GetSceneItemProperties', {"item": process.env.OBS_ITEMCAMERA}, (err, data) => {
-            if (err && err.status === "error" && err.error === "specified scene item doesn't exist") {
-                client.say(process.env.KANALNAME, "Diese Quelle existiert nicht.");
-                return false;
-            }
-            if (data && data.status === "ok") {
-                return client.say(process.env.KANALNAME, `Kameraposition: x: ${data.position.x}; y: ${data.position.y}`)
-            }
+            if (err && err.status === "error" && err.error === "specified scene item doesn't exist") return client.say(process.env.KANALNAME, "Diese Quelle existiert nicht.");
+            if (data && data.status === "ok") return client.say(process.env.KANALNAME, `Kameraposition: x: ${data.position.x}; y: ${data.position.y}`);
         });
         return true;
     }
